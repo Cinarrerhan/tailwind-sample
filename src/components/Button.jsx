a75d3ffc552de702a1093b0b593743ebb9503696
Erhan <cinarerhan94@gmail.com>
2023-09-17 22:20:12 +0300

    added superQuality component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,11 +6,13 @@ const Button = ({ label, iconURL }) => {
     rounded-full text-white border-coral-red"
     >
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
