9d5d35ba2ec901bed9e69734278dd305d05b9171
Erhan <cinarerhan94@gmail.com>
2023-09-17 10:33:08 +0300

    shoe card dev

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,6 @@
 import Button from "../components/Button";
+
+import { useState } from "react";
 import ShoeCard from "../components/ShoeCard";
 
 import { arrowRight } from "../assets/icons";
@@ -6,6 +8,8 @@ import { statistics, shoes } from "../constants";
 import { bigShoe1 } from "../assets/images";
 
 const Hero = () => {
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
   return (
     <section
       id="home"
@@ -52,16 +56,23 @@ const Hero = () => {
       bg-hero bg-cover bg-center"
       >
         <img
-          src={bigShoe1}
+          src={bigShoeImg}
           alt="shoe collection"
           width={610}
           height={500}
           className="object-contain relative z-10"
         />
-        <div>
+        <div
+          className="flex sm:gap-6 gap-4 absolute -bottom-[5%]
+        sm:left-[10%] max-sm:px-6"
+        >
           {shoes.map((shoe, index) => (
             <div key={shoe}>
-              <ShoeCard />
+              <ShoeCard
+                imgURL={shoe}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
             </div>
           ))}
         </div>
